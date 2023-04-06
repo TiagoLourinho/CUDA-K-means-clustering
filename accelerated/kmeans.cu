c98cbf57fdf70d05bbc11418dced395edb66412b
Tiago Lourinho <tiagolourinho001@gmail.com>
2023-04-06 11:07:22 +0100

    changed accelereted part do .cu

--- accelerated/kmeans.cu
+++ accelerated/kmeans.cu
@@ -17,7 +17,7 @@
 /*                                                                           */
 /*2       Redistributions in binary form must reproduce the above copyright   */
 /*        notice, this list of conditions and the following disclaimer in the */
-/*        documentation and/or other materials provided with the distribution.*/ 
+/*        documentation and/or other materials provided with the distribution.*/
 /*                                                                            */
 /*3       Neither the name of Northwestern University nor the names of its    */
 /*        contributors may be used to endorse or promote products derived     */
@@ -75,6 +75,7 @@
 #include <sys/types.h>
 #include <fcntl.h>
 #include <time.h>
+#include <unistd.h>
 #include "getopt.h"
 
 #include "kmeans.h"
@@ -82,86 +83,99 @@
 extern double wtime(void);
 
 /*---< usage() >------------------------------------------------------------*/
-void usage(char *argv0) {
+void usage(char *argv0)
+{
     char *help =
         "Usage: %s [switches] -i filename\n"
         "       -i filename     :  file containing data to be clustered\n"
         "       -b                 :input file is in binary format\n"
-		"       -k                 : number of clusters (default is 8) \n"
+        "       -k                 : number of clusters (default is 8) \n"
         "       -t threshold    : threshold value\n";
     fprintf(stderr, help, argv0);
     exit(-1);
 }
 
 /*---< main() >-------------------------------------------------------------*/
-int main(int argc, char **argv) {
-           int     opt;
-    extern char   *optarg;
-    extern int     optind;
-           int     nclusters=5;
-           char   *filename = 0;           
-           float  *buf;
-           float **attributes;
-           float **cluster_centres=NULL;
-           int     i, j;           
-		   
-           int     numAttributes;
-           int     numObjects;           
-           char    line[1024];
-           int     isBinaryFile = 0;
-           int     nloops;
-           float   threshold = 0.001;
-		   double  timing;
-       struct timespec t1, t2;
+int main(int argc, char **argv)
+{
+    int opt;
+    extern char *optarg;
+    extern int optind;
+    int nclusters = 5;
+    char *filename = 0;
+    float *buf;
+    float **attributes;
+    float **cluster_centres = NULL;
+    int i, j;
 
+    int numAttributes;
+    int numObjects;
+    char line[1024];
+    int isBinaryFile = 0;
+    int nloops;
+    float threshold = 0.001;
+    double timing;
+    struct timespec t1, t2;
 
-	while ( (opt=getopt(argc,argv,"i:k:t:b"))!= EOF) {
-        switch (opt) {
-            case 'i': filename=optarg;
-                      break;
-            case 'b': isBinaryFile = 1;
-                      break;
-            case 't': threshold=atof(optarg);
-                      break;
-            case 'k': nclusters = atoi(optarg);
-                      break;
-            case '?': usage(argv[0]);
-                      break;
-            default: usage(argv[0]);
-                      break;
+    while ((opt = getopt(argc, argv, "i:k:t:b")) != EOF)
+    {
+        switch (opt)
+        {
+        case 'i':
+            filename = optarg;
+            break;
+        case 'b':
+            isBinaryFile = 1;
+            break;
+        case 't':
+            threshold = atof(optarg);
+            break;
+        case 'k':
+            nclusters = atoi(optarg);
+            break;
+        case '?':
+            usage(argv[0]);
+            break;
+        default:
+            usage(argv[0]);
+            break;
         }
     }
 
-    if (filename == 0) usage(argv[0]);
+    if (filename == 0)
+        usage(argv[0]);
 
     numAttributes = numObjects = 0;
 
     /* from the input file, get the numAttributes and numObjects ------------*/
-   
-    if (isBinaryFile) {
+
+    if (isBinaryFile)
+    {
         int infile;
-        if ((infile = open(filename, O_RDONLY, "0600")) == -1) {
+        if ((infile = open(filename, O_RDONLY, "0600")) == -1)
+        {
             fprintf(stderr, "Error: no such file (%s)\n", filename);
             exit(1);
         }
-        read(infile, &numObjects,    sizeof(int));
+        read(infile, &numObjects, sizeof(int));
         read(infile, &numAttributes, sizeof(int));
-   
 
         /* allocate space for attributes[] and read attributes of all objects */
-        buf           = (float*) malloc(numObjects*numAttributes*sizeof(float));
-        attributes    = (float**)malloc(numObjects*             sizeof(float*));
-        attributes[0] = (float*) malloc(numObjects*numAttributes*sizeof(float));
-        for (i=1; i<numObjects; i++)
-            attributes[i] = attributes[i-1] + numAttributes;
+        buf = (float *)malloc(numObjects * numAttributes * sizeof(float));
+        attributes = (float **)malloc(numObjects * sizeof(float *));
+        attributes[0] = (float *)malloc(numObjects * numAttributes * sizeof(float));
+        for (i = 1; i < numObjects; i++)
+            attributes[i] = attributes[i - 1] + numAttributes;
 
-        read(infile, buf, numObjects*numAttributes*sizeof(float));
+        read(infile, buf, numObjects * numAttributes * sizeof(float));
 
         close(infile);
     }
-    else {
+    else
+    {
         FILE *infile;
-        if ((infile = fopen(filename, "r")) == NULL) {
+        if ((infile = fopen(filename, "r")) == NULL)
+        {
             fprintf(stderr, "Error: no such file (%s)\n", filename);
             exit(1);
         }
@@ -169,74 +183,76 @@ int main(int argc, char **argv) {
             if (strtok(line, " \t\n") != 0)
                 numObjects++;
         rewind(infile);
-        while (fgets(line, 1024, infile) != NULL) {
-            if (strtok(line, " \t\n") != 0) {
+        while (fgets(line, 1024, infile) != NULL)
+        {
+            if (strtok(line, " \t\n") != 0)
+            {
                 /* ignore the id (first attribute): numAttributes = 1; */
-                while (strtok(NULL, " ,\t\n") != NULL) numAttributes++;
+                while (strtok(NULL, " ,\t\n") != NULL)
+                    numAttributes++;
                 break;
             }
         }
-     
 
         /* allocate space for attributes[] and read attributes of all objects */
-        buf           = (float*) malloc(numObjects*numAttributes*sizeof(float));
-        attributes    = (float**)malloc(numObjects*             sizeof(float*));
-        attributes[0] = (float*) malloc(numObjects*numAttributes*sizeof(float));
-        for (i=1; i<numObjects; i++)
-            attributes[i] = attributes[i-1] + numAttributes;
+        buf = (float *)malloc(numObjects * numAttributes * sizeof(float));
+        attributes = (float **)malloc(numObjects * sizeof(float *));
+        attributes[0] = (float *)malloc(numObjects * numAttributes * sizeof(float));
+        for (i = 1; i < numObjects; i++)
+            attributes[i] = attributes[i - 1] + numAttributes;
         rewind(infile);
         i = 0;
-        while (fgets(line, 1024, infile) != NULL) {
-            if (strtok(line, " \t\n") == NULL) continue; 
-            for (j=0; j<numAttributes; j++) {
+        while (fgets(line, 1024, infile) != NULL)
+        {
+            if (strtok(line, " \t\n") == NULL)
+                continue;
+            for (j = 0; j < numAttributes; j++)
+            {
                 buf[i] = atof(strtok(NULL, " ,\t\n"));
                 i++;
             }
         }
         fclose(infile);
     }
-  
-    nloops = 1;	
-	printf("I/O completed\n");
 
-	memcpy(attributes[0], buf, numObjects*numAttributes*sizeof(float));
+    nloops = 1;
+    printf("I/O completed\n");
+
+    memcpy(attributes[0], buf, numObjects * numAttributes * sizeof(float));
 
     clock_gettime(CLOCK_REALTIME, &t1);
-    for (i=0; i<nloops; i++) {
-        		
+    for (i = 0; i < nloops; i++)
+    {
+
         cluster_centres = NULL;
         cluster(numObjects,
                 numAttributes,
-                attributes,           /* [numObjects][numAttributes] */
+                attributes, /* [numObjects][numAttributes] */
                 nclusters,
                 threshold,
-                &cluster_centres   
-               );
-
-     
+                &cluster_centres);
     }
     clock_gettime(CLOCK_REALTIME, &t2);
 
-  timing = (((double)t2.tv_sec-t1.tv_sec)*1000.0) + (((double)t2.tv_nsec-t1.tv_nsec)/1000000.0);
+    timing = (((double)t2.tv_sec - t1.tv_sec) * 1000.0) + (((double)t2.tv_nsec - t1.tv_nsec) / 1000000.0);
 
-	printf("number of Clusters %d\n",nclusters); 
-	printf("number of Attributes %d\n\n",numAttributes); 
-    /*printf("Cluster Centers Output\n"); 
-	printf("The first number is cluster number and the following data is arribute value\n");
-	printf("=============================================================================\n\n");
-	
+    printf("number of Clusters %d\n", nclusters);
+    printf("number of Attributes %d\n\n", numAttributes);
+    /*printf("Cluster Centers Output\n");
+    printf("The first number is cluster number and the following data is arribute value\n");
+    printf("=============================================================================\n\n");
+
     for (i=0; i<nclusters; i++) {
-		printf("%d: ", i);
+        printf("%d: ", i);
         for (j=0; j<numAttributes; j++)
             printf("%f ", cluster_centres[i][j]);
         printf("\n\n");
     }*/
-	printf("Time for process: %f [ms]\n", timing);
+    printf("Time for process: %f [ms]\n", timing);
 
     free(attributes);
     free(cluster_centres[0]);
     free(cluster_centres);
     free(buf);
-    return(0);
+    return (0);
 }
-
